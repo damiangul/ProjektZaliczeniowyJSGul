85b1cc8eb27de9b7c2ecb44fca9de6d38fa1d86f
adriankleyna <61495980+adriankleyna@users.noreply.github.com>
2021-01-30 19:33:35 +0100

    animation worked now
    worked

--- public/js/ourTeamAnimation.js
+++ public/js/ourTeamAnimation.js
@@ -1,7 +1,8 @@
-var pathEls = document.querySelectorAll('path');
-for (var i = 0; i < pathEls.length; i++) {
-    var pathEl = pathEls[i];
-    var offset = anime.setDashoffset(pathEl);
+// let pathEls = document.querySelectorAll("#ourTeamAnimationPath. path");
+let pathEls = document.querySelectorAll("#SecondAnimation .SecondAnimation_g path");
+for (let i = 0; i < pathEls.length; i++) {
+    let pathEl = pathEls[i];
+    let offset = anime.setDashoffset(pathEl);
     pathEl.setAttribute('stroke-dashoffset', offset);
     anime({
         targets: pathEl,
@@ -14,3 +15,14 @@ for (var i = 0; i < pathEls.length; i++) {
         autoplay: true
     });
 }
+
+// let ourTeamAnimationDrawing = anime({
+//     targets: "#SecondAnimation .SecondAnimation_g path",
+//     strokeDashoffset: [offset, 0],
+//     duration: anime.random(2000, 6000),
+//     delay: anime.random(0, 4000),
+//     loop: true,
+//     direction: 'alternate',
+//     easing: 'easeInOutSine',
+//     autoplay: true
+// });
