cfea75d9dc782ba0f0a7562b97c69ac09ea8dfc4
Damian Gul <damian.robert.gul@gmail.com>
2021-01-30 20:25:36 +0100

    Final improvments
    Fixed code formatting and fixed animation on page about us
    
    fix fix :)

--- public/js/ourTeamAnimation.js
+++ public/js/ourTeamAnimation.js
@@ -1,4 +1,3 @@
-// let pathEls = document.querySelectorAll("#ourTeamAnimationPath. path");
 let pathEls = document.querySelectorAll("#SecondAnimation .SecondAnimation_g path");
 for (let i = 0; i < pathEls.length; i++) {
     let pathEl = pathEls[i];
@@ -15,14 +14,3 @@ for (let i = 0; i < pathEls.length; i++) {
         autoplay: true
     });
 }
-
-// let ourTeamAnimationDrawing = anime({
-//     targets: "#SecondAnimation .SecondAnimation_g path",
-//     strokeDashoffset: [offset, 0],
-//     duration: anime.random(2000, 6000),
-//     delay: anime.random(0, 4000),
-//     loop: true,
-//     direction: 'alternate',
-//     easing: 'easeInOutSine',
-//     autoplay: true
-// });
